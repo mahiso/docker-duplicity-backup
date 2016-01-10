46f2b764018e926427090a8d35566d455ba09612
Maik Hinrichs <maik@mahiso.de>
2016-01-10 15:43:20 +0100

    Creation of log and cache dir; remove of old docker containers during start

--- duplicity-backup.sh
+++ duplicity-backup.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 MYPATH=`dirname $(readlink -f $0)`
 MYIMAGE='mahiso/duplicity-backup'
+
+# Create log and cache dir's
+mkdir -p $MYPATH/backup_var/log $MYPATH/backup_var/cache
+
 # Remove old containers
-#docker rm $(docker ps -a | grep $MYIMAGE | awk '{print $1}') >/dev/null
+docker rm $(docker ps -a | grep $MYIMAGE | awk '{print $1}') >/dev/null
+
 # Run it
 docker run -ti -v $MYPATH/backup_var:/backup_var -v /:/SRC $MYIMAGE -c /backup_var/duplicity-backup.conf "$@"
